af0b6b92d3174808a9299b88a6e0c0ade323325a
andyverstraeten <andy.verstraeten@gmail.com>
2024-10-06 20:40:11 -0500

    output file validation added

--- mdexport.py
+++ mdexport.py
@@ -2,9 +2,15 @@ import click
 from pathlib import Path
 
 
+def validate_output_file(ctx: click.Context, param: click.Option, value: str):
+    if ".pdf" not in value:
+        raise click.BadParameter("Only export to pdf format is supported.")
+
+
 @click.command()
 @click.argument("md_file")
-def pdfgen(md_file: Path) -> None:
+@click.option("--output", "-o", required=True, type=str, callback=validate_output_file)
+def pdfgen(md_file: Path, output: str) -> None:
     print(f"hello {md_file}")
 
 
