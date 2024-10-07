90a4288e60f9b11f772d6afe4ee974cda22c0ac4
andyverstraeten <andy.verstraeten@gmail.com>
2024-10-06 20:43:37 -0500

    added validate md input file

--- mdexport.py
+++ mdexport.py
@@ -7,11 +7,16 @@ def validate_output_file(ctx: click.Context, param: click.Option, value: str):
         raise click.BadParameter("Only export to pdf format is supported.")
 
 
+def validate_md_file(ctx: click.Context, param: click.Option, value: str):
+    if ".md" not in value:
+        raise click.BadParameter("Only markdown(.md) files are supported as input.")
+
+
 @click.command()
-@click.argument("md_file")
+@click.argument("markdown_file", type=str, callback=validate_md_file)
 @click.option("--output", "-o", required=True, type=str, callback=validate_output_file)
-def pdfgen(md_file: Path, output: str) -> None:
-    print(f"hello {md_file}")
+def pdfgen(markdown_file: str, output: str) -> None:
+    print(f"hello {markdown_file}")
 
 
 if __name__ == "__main__":
