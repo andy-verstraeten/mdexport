92c7a981c4c89f614e27d86083ff97dbe52db361
andyverstraeten <andy.verstraeten@gmail.com>
2024-11-06 19:46:54 -0500

    publishing error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mdexport",
-    version="0.0.1",
+    version="0.0.2",
     py_modules=find_packages(),
     install_requires=[
         "click",
@@ -13,5 +13,5 @@ setup(
         "pytest",
         "pytest-cov",
     ],
-    entry_points={"console_scripts": ["mdexport = mdexport.mdexport:cli"]},
+    entry_points={"console_scripts": ["mdexport = mdexport:cli"]},
 )
