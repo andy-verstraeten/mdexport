e5a9ffc532e238994e8f9787961011a2ce73aa38
andyverstraeten <andy.verstraeten@gmail.com>
2024-11-24 10:06:36 -0500

    added tests for the Config class

--- mdexport/config.py
+++ mdexport/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import json
 import click
-
+from sys import exit
 
 APP_NAME = "mdexport"
 CONFIG_FILENAME = "config.json"
--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,12 @@ from pathlib import Path
 from mdexport.config import (
     _get_config_directory,
     APP_NAME,
+    CONFIG_FILENAME,
+    Config,
+    ConfigStructure,
 )
+import mdexport.config
+import sys
 
 
 def test_config_directory_windows(monkeypatch: MonkeyPatch, tmp_path: Path):
@@ -21,4 +26,86 @@ def test_config_directory_unix(monkeypatch: MonkeyPatch, tmp_path: Path):
     assert config_dir == tmp_path / ".config" / APP_NAME
 
 
-# TODO: implement more tests here
+def test_config_init(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+    Config()
+    assert (tmp_path / CONFIG_FILENAME).is_file()
+
+
+class MockConfigStructure:
+    CONFIG1 = "config1"
+    CONFIG2 = "config2"
+    CONFIG3 = "config3"
+
+
+def test_config_load(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+    monkeypatch.setattr(mdexport.config, "DEFAULT_CONFIG", {"config3": "value3"})
+    monkeypatch.setattr(mdexport.config, "ConfigStructure", MockConfigStructure)
+    (tmp_path / CONFIG_FILENAME).write_text("""{"config1":"value1",
+"config2":"value2"
+}""")
+    config = Config()
+    config.load()
+    assert config.config == {
+        "config1": "value1",
+        "config2": "value2",
+        "config3": "value3",
+    }
+
+
+def test_config_save(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+    config = Config()
+    config.config = {"config1": "value1", "config2": "value2", "config3": "value3"}
+    config.save()
+    assert (
+        tmp_path / CONFIG_FILENAME
+    ).read_text() == '{"config1": "value1", "config2": "value2", "config3": "value3"}'
+
+
+def test_config_set(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+    monkeypatch.setattr(mdexport.config, "ConfigStructure", MockConfigStructure)
+    savecalled = False
+
+    def mock_save(_):
+        nonlocal savecalled
+        savecalled = True
+
+    monkeypatch.setattr(mdexport.config.Config, "save", mock_save)
+    config = Config()
+    config.set("config1", "value2")
+    assert config.config == {"config1": "value2"} and savecalled
+
+
+def test_config_preflight_templatedir_not_set(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+
+    exit_called = False
+
+    def mock_exit():
+        nonlocal exit_called
+        exit_called = True
+
+    monkeypatch.setattr(mdexport.config, "exit", mock_exit)
+    config = Config()
+    config.config = {ConfigStructure.TEMPLATE_DIR: ""}
+    config.pre_publish_config_check()
+    assert exit_called
+
+
+def test_config_preflight_templatedir_invalid(monkeypatch: MonkeyPatch, tmp_path: Path):
+    monkeypatch.setattr(mdexport.config, "_get_config_directory", lambda: tmp_path)
+
+    exit_called = False
+
+    def mock_exit():
+        nonlocal exit_called
+        exit_called = True
+
+    monkeypatch.setattr(mdexport.config, "exit", mock_exit)
+    config = Config()
+    config.config = {ConfigStructure.TEMPLATE_DIR: (tmp_path / "not_a_dir")}
+    config.pre_publish_config_check()
+    assert exit_called
